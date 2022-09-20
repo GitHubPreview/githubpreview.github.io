fbbd2be0fa422d2587ceb1c1741a737417ba1304
GitHubPreview <114020107+GitHubPreview@users.noreply.github.com>
2022-09-20 13:45:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const err = urlParams.get('err');
+
+if (err) {
+  alert('Error: ' + err);
+}
+
+function html() {
+  if (isValidHttpUrl(document.getElementById('url').value)) {
+    location.href = location.href.replace(location.search, '').replace('index.html', 'html.html') + '?url=' + document.getElementById('url').value;
+  } else {
+    location.href = location.href.replace(location.search, '').replace('index.html', 'html.html') + '?user=' + document.getElementById('repo').value.split('/')[0] +'&repo=' + document.getElementById('repo').value.split('/')[1] +'&branch=' + document.getElementById('repo').value.split('/')[2] +'&file=' + document.getElementById('repo').value.split('/')[3];
+  }
+}
+
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
